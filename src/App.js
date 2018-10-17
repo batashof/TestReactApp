8c07375eeaa68b37f6b3eb68389b88994b55affc
batashof <batashof@gmail.com>
2018-10-17 12:29:35 +0300

    app done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: []
+    };
+  }
+
+
+  componentDidMount() {
+    let IDs = [2, 7, 1, 8, 3, 9];
+    for (let i = 0; i < IDs.length; i++) {
+      fetch(`https://picurl.herokuapp.com/users/${IDs[i]}`)
+          .then(response => response.json())
+          .then(data => this.setState  ({
+            users: [...this.state.users, data.url]
+          }));
+    }
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+        <div className="App">
+          {this.state.users.map(user => (
+              <div >
+                <img  className="Img"  src={user} alt="img"/>
+              </div>
+          ))}
+        </div>
+    )
   }
 }
 
